85bb473e970b37c1050c23783405c8c3c3876c76
Justice Rivera <142175716+justicetrace@users.noreply.github.com>
2024-07-28 23:29:04 +0000

    working on character card

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,57 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
+
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+
+<div className="text-center mt-5">
+<Welcome/>
+</div>
+
+
+  /*<div className="container-fluid overflow-x-auto">
+    <div className="row">
+      <h1 className="text-danger">Characters</h1>
+      <div className="card w-25 mx-3">
+        <div className="card-body">
+          <img src="..." className="card-img-top" alt="..." />
+          <h5 className="card-title">Luke Skywalker</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-info">Learn More!</a>
+        </div>
+      </div>
+      <div className="card w-25 mx-3">
+        <div className="card-body">
+          <img src="..." className="card-img-top" alt="..." />
+          <h5 className="card-title">Luke Skywalker</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-info">Learn More!</a>
+        </div>
+      </div>
+      <div className="card w-25 mx-3">
+        <div className="card-body">
+          <img src="..." className="card-img-top" alt="..." />
+          <h5 className="card-title">Luke Skywalker</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-info">Learn More!</a>
+        </div>
+      </div>
+      <div className="card w-25 mx-3">
+        <div className="card-body">
+          <img src="..." className="card-img-top" alt="..." />
+          <h5 className="card-title">Han Solo</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-info">Learn More!</a>
+        </div>
+      </div>
+      <div className="card w-25 mx-3">
+        <div className="card-body">
+          <img src="..." className="card-img-top" alt="..." />
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-info">Learn More!</a>
+        </div>
+      </div>
+    </div>
+  </div> */
 );
